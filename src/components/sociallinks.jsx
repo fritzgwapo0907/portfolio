34c67ef6582ec739c59e98cbff0cc30cbd83a0bd
unknown <fritztacanay0907@gmail.com>
2025-05-07 20:33:05 +0800

    links

--- src/components/sociallinks.jsx
+++ src/components/sociallinks.jsx
@@ -4,7 +4,7 @@ export default function SocialLinks() {
         <h2>Connect with Me</h2>
         <ul>
           <li><a href="https://github.com/fritzgwapo0907" target="_blank">GitHub</a></li>
-          <li><a href="https://linkedin.com/in/yourusername" target="_blank">LinkedIn</a></li>
+          <li><a href="https://www.linkedin.com/in/fritz-gerald-tacanay-6973a5361/" target="_blank">LinkedIn</a></li>
           <li><a >fritztacanay0907@gmail.com</a></li>
           <li><a href="https://web.facebook.com/fritztacanay/">Facebook</a></li>
         </ul>
