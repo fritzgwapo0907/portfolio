86b7b36c1926810f3ecac42707ae7d644dcefa45
unknown <fritztacanay0907@gmail.com>
2025-05-07 20:01:46 +0800

    pics hotodog

--- src/components/projects.jsx
+++ src/components/projects.jsx
@@ -8,19 +8,19 @@ const projects = [
   {
     title: 'ARRAY_OBJECTS',
     description: 'A full-stack task management app with user authentication.',
-    imageUrl: '/screenshot_2025-05-07_182308.png',
+    imageUrl: '/screenshot_2025-05-07_182353.png',
     link: 'array_objects/index.html', // Replace with your project link
   },
   {
     title: 'CONVERTER',
     description: 'An e-commerce platform built with Node.js and Express.',
-    imageUrl: '/screenshot_2025-05-07_182308.png',
+    imageUrl: '/screenshot_2025-05-07_182414.png',
     link: 'converter/index.html', // Replace with your project link
   },
   {
     title: 'MATH OPERATION',
     description: 'A simple blog platform with CRUD functionality.',
-    imageUrl: '/screenshot_2025-05-07_182308.png',
+    imageUrl: '/screenshot_2025-05-07_182434.png',
     link: 'loop/index.html', // Replace with your project link
   },
   {
